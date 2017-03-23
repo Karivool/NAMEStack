87c31c4e9c918b44e8590b87728c1edd71d02c49
Karivool <themataaa@gmail.com>
2017-03-23 02:19:13 -0400

    Add dummy data to controller

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,3 +1,27 @@
-function AppController() {
+function AppController($scope) {
   console.log("Controller initialized");
+
+  chr1 = {
+    name: "Pizzer",
+    hp: 2900,
+    atk: 1650,
+    rcv: 444
+  };
+
+  chr2 = {
+    name: "Brooker",
+    hp: 2800,
+    atk: 1400,
+    rcv: 751
+  };
+
+  chr3 = {
+    name: "Lifter",
+    hp: 2200,
+    atk: 2100,
+    rcv: -100,
+  };
+
+  let cards = [chr1, chr2, chr3];
+  $scope.cards = cards;
 }
