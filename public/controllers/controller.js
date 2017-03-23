b32d01d981f75459e7dd24f3d510cbe5545924b4
Karivool <themataaa@gmail.com>
2017-03-23 03:12:50 -0400

    Update public items

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,27 +1,8 @@
-function AppController($scope) {
+function AppController($scope, $http) {
   console.log("Controller initialized");
 
-  chr1 = {
-    name: "Pizzer",
-    hp: 2900,
-    atk: 1650,
-    rcv: 444
-  };
-
-  chr2 = {
-    name: "Brooker",
-    hp: 2800,
-    atk: 1400,
-    rcv: 751
-  };
-
-  chr3 = {
-    name: "Lifter",
-    hp: 2200,
-    atk: 2100,
-    rcv: -100,
-  };
-
-  let cards = [chr1, chr2, chr3];
-  $scope.cards = cards;
+  $http.get('/cardinfo').success(function(response) {
+    console.log("Data received successfully.");
+    $scope.cardinfo = response;
+  });
 }
