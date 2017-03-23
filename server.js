a6da091ae72be2f3ab033a7f4daeddb9913a4c04
Karivool <themataaa@gmail.com>
2017-03-23 03:12:37 -0400

    Update server with dummy params

--- server.js
+++ server.js
@@ -3,6 +3,34 @@ let app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/cardinfo', function (req, res) {
+  console.log("GET request made.");
+
+  chr1 = {
+    name: "Pizzer",
+    hp: 2900,
+    atk: 1650,
+    rcv: 444
+  };
+
+  chr2 = {
+    name: "Brooker",
+    hp: 2800,
+    atk: 1400,
+    rcv: 751
+  };
+
+  chr3 = {
+    name: "Lifter",
+    hp: 2200,
+    atk: 2100,
+    rcv: -100,
+  };
+
+  let cardinfo = [chr1, chr2, chr3];
+  res.json(cardinfo);
+});
+
 let port = 3000;
 
 app.listen(port);
