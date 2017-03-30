e60d7c678ab9c81a071eba0ff2a52498a71b2451
Karivool <themataaa@gmail.com>
2017-03-30 03:06:53 -0400

    Update public files

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,4 +5,9 @@ function AppController($scope, $http) {
     console.log("Data received successfully.");
     $scope.cardinfo = response;
   });
+
+  $scope.addCard = function() {
+    console.log($scope.card);
+    $http.post("/cardinfo", $scope.card);
+  };
 }
