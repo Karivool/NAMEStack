0836b855edfbff06e76bd6a10cbf07663aaca293
Karivool <themataaa@gmail.com>
2017-04-03 04:52:39 -0400

    Remove public console.log()'s

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -6,7 +6,6 @@ function AppController($scope, $http) {
       method: 'GET',
       url: '/cardinfo/'
     }).then(function success(response) {
-      console.log("Data received successfully.");
       $scope.cardinfo = response;
       $scope.card = "";
     }, function error(response) {
@@ -17,30 +16,24 @@ function AppController($scope, $http) {
   refresh();
 
   $scope.addCard = function() {
-    console.log($scope.card);
     $http.post('/cardinfo', $scope.card).success(function(response) {
-      console.log(response);
-      console.log("POST Success");
       refresh();
     });
   };
 
   $scope.remove = function(id) {
-    console.log(id);
     $http.delete('/cardinfo/' + id).success(function(response) {
       refresh();
     });
   };
 
   $scope.edit = function(id) {
-    console.log(id);
     $http.get('/cardinfo/' + id).success(function(response) {
       $scope.card = response;
     });
   };
 
   $scope.update = function() {
-    console.log($scope.card._id);
     $http.put('/cardinfo/' + $scope.card._id, $scope.card).success(function(response) {
       refresh();
     });
